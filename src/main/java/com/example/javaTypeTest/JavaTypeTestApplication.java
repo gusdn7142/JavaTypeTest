4b5f83faa000cce4048f08197d500bc1ba8b1cc0
gusdn7142 <gusdnchl7144@naver.com>
2024-11-24 22:24:07 +0900

    first commit

---
+++ src/main/java/com/example/javaTypeTest/JavaTypeTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.javaTypeTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavaTypeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTypeTestApplication.class, args);
+	}
+
+}
